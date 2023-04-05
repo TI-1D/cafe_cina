877ab15a3bd3996a69c12ccac6018469c574c3e8
Agrama26 <ramadhanagung1112@gmail.com>
2023-04-05 20:41:20 +0700

    Kelompok_2

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+
+// Jika Burger menu di klik
+document.querySelector("#burger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Klik diluar sidebar untuk menghilangkan nav
+const burger = document.querySelector("#burger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!burger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
